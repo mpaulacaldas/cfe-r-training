7ce48e5983c914b2a1350302e8778e521f13cd29
Maria Paula Caldas <mpaulacaldas@gmail.com>
2021-09-10 23:34:10 +0200

    Create slide copies from template

--- docs/_setup.R
+++ docs/_setup.R
@@ -1,2 +1,15 @@
+devtools::install_github("gadenbuie/xaringanExtra")
 remotes::install_github("mitchelloharawild/icons")
 icons::download_fontawesome()
+
+library(purrr)
+c(
+  "01_intro.Rmd",
+  "02_dataviz.Rmd",
+  "03_programming.Rmd",
+  "04_rmarkdown.Rmd",
+  "05_goodpractice.Rmd"
+  ) %>%
+  fs::path("docs/", .) %>%
+  walk(~ fs::file_copy("docs/_template.Rmd", .x))
+
